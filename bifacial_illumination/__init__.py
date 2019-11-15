d3242701aa28f8f34eb8fab7a4cdd2b29ada418c
Peter Tillmann <Peter.tillmann@helmholtz-berlin.de>
2019-11-15 15:24:25 +0100

    cleanup and restructed folder. Added code for publication figures

---
+++ bifacial_illumination/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from bifacial_illumination.geo import ModuleIllumination
+
+__version__ = '0.0.1'
+__all__ = ['ModuleIllumination']
--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ def readme():
         return f.read()
 
 
-setup(name='bifacial',
-      version='0.0.3',
+setup(name='bifacial_illumination',
+      version='0.0.1',
       description='''This package can be used to simulate the irradiance of direct and diffuse light onto a bifacial solar cell.''',
       #long_description=readme(),
-      url='https://github.com/P-Tillmann/bifacial',
-      author='HZB Nano-Sippe Group',
+      url='https://github.com/nano-sippe/bifacial_illumination',
+      author='Peter Tillmann, Klaus Jäger',
       author_email='Peter.tillmann@helmholtz-berlin.de',
       license='MIT',
-      packages=['bifacial_geo'],
+      packages=['bifacial_illumination'],
       #setup_requires=['pytest-runner'],
       #test_require=['pytest'],
       install_requires=[
-          'numpy', 'scikit-optimize', 'pandas', 'seaborn', 'joblib'
+          'numpy'
       ],
       zip_safe=False)
