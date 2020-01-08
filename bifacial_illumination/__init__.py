c7bedc05733fe4796b74e410e8cdfa68fea4c7e5
Peter Tillmann <Peter.tillmann@helmholtz-berlin.de>
2020-01-08 14:21:37 +0100

    updated init

--- bifacial_illumination/__init__.py
+++ bifacial_illumination/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from bifacial_illumination.geo import ModuleIllumination
+from illumination import Illumination
+from tmy_yield import YieldSimulator, CostOptimizer
 
 __version__ = '0.0.1'
-__all__ = ['ModuleIllumination']
+__all__ = ['ModuleIllumination', 'Illumination', 'YieldSimulator', 'CostOptimizer']
